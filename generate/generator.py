fa5945efb0c6b751fb13db2e8ee30093964bbee9
shevawen <sheva.wen@gmail.com>
2015-08-13 09:39:51 +0800

    ...

--- generate/generator.py
+++ generate/generator.py
@@ -3,8 +3,9 @@ from xml.dom.minidom import parse
 import xml.dom.minidom
 from string import Template
 import json
-#import cairo
-#import rsvg
+import cairocffi as cairo
+import cairosvg
+
 
 file_warning = open( 'warnings.json')
 warnings = json.loads(file_warning.read(  ).decode('utf8'))
@@ -18,9 +19,9 @@ for warning in warnings['warnings']:
     icon_svg_dom = xml.dom.minidom.parse(warning['weather_icon'])
     icon_path = icon_svg_dom.getElementsByTagName('path')[0].getAttributeNode('d').nodeValue
     for color in warning['colors']:
-        output_path_prefix = '../set1/svg/' + warning['weather_en'] + '_' + color
-        output_path_svg = output_path_prefix + '.svg'
-        output_path_png = output_path_prefix + '.png'
+        output_path_temp = '../set1/{format}/' + warning['weather_en'].replace(' ','') + '_' + color + '.{format}'
+        output_path_svg = output_path_temp.format(format = 'svg')
+        output_path_png = output_path_temp.format(format = 'png')
         d = {\
         'color_hex': color_mapping[color]['hex'], \
         'color_text': color_mapping[color]['zh'] + u'预警', \
@@ -31,4 +32,4 @@ for warning in warnings['warnings']:
         svg_src = src.substitute(d).encode('utf8');
         output = open(output_path_svg,'w')
         output.write(svg_src)
-        #cairosvg.svg2png(bytestring=svg_src, write_to=output_path_png)
+        cairosvg.svg2png(bytestring=svg_src, write_to=output_path_png)
