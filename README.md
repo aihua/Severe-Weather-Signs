780d1a30eb6840ae8221b9ea6b93efb0fc558edc
shevawen <sheva.wen@gmail.com>
2015-08-17 22:54:01 +0800

    convert svg to png using content-screenshot

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Emergent-Meteorological-Disaster-Warning-Sgnal-Icon
-An icon set using Google Material Design. An unoffical version of the CMA's icon set. 
+An icon set using Google Material Design. An unoffical version of the CMA's icon set.
 
 See http://www.cma.gov.cn/2011zwxx/2011zflfg/2011zgfxwj/201110/t20111027_134927.html
 
 依照 Google Material Design 制作一套非官方的“突发气象灾害预警信号”图标，与现代软件界面相适应。
 
 ## 暴雨
+
+![暴雨](https://raw.githubusercontent.com/shevawen/Emergent-Meteorological-Disaster-Warning-Sgnal-Icon/master/set1/png/RAINSTORM_red.png)
+
 ## 暴雪
 ## 寒潮
 ## 大风
--- generate/generator.py
+++ generate/generator.py
@@ -3,9 +3,8 @@ from xml.dom.minidom import parse
 import xml.dom.minidom
 from string import Template
 import json
-import cairocffi as cairo
-import cairosvg
-
+import subprocess
+import os
 
 file_warning = open( 'warnings.json')
 warnings = json.loads(file_warning.read(  ).decode('utf8'))
@@ -25,11 +24,19 @@ for warning in warnings['warnings']:
         d = {\
         'color_hex': color_mapping[color]['hex'], \
         'color_text': color_mapping[color]['zh'] + u'预警', \
-        'weather':warning['weather'],\
+        'weather': warning['weather'],\
         'weather_en': warning['weather_en'],\
         'weather_icon_path': icon_path\
         }
         svg_src = src.substitute(d).encode('utf8');
-        output = open(output_path_svg,'w')
-        output.write(svg_src)
-        cairosvg.svg2png(bytestring=svg_src, write_to=output_path_png)
+        with open(output_path_svg, "wb") as outputFile:
+            outputFile.write(svg_src)
+            print "SVG Output Done"
+        command = ['./content-screenshot/contentScreenshot.py', os.path.abspath(output_path_svg), os.path.abspath(output_path_png)]
+        p = subprocess.Popen(command, shell = False, stdout = subprocess.PIPE,
+                             stderr = subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        if (stdout.startswith("Done")):
+            print "SVG Convert Done"
+        else:
+            print stderr
